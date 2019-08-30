281a31adcbedd717dc4f998e87fa7a5d617b5d10
James Knight <james.p.knight2@gmail.com>
2019-08-30 07:06:44 -0600

    Create countdown_clock.rb
    Sometimes get bored in class and calculate what percentage done with a lecture I am (sorry professors!)
    During an especially boring english lecture I opened up vim and made this countdown timer that allows you to enter an amount of time to countdown and set an alert interval i.e. countdown 1 hour and 40 minutes and alert me of my percentage complete every minute
    input
    ```
    Countdown Timer
    How many Hours:
    1
    How many Minutes:
    40
    How many seconds:
    0
    How many seconds between alerts?
    60
    
    ```
    output(every minute) 
    ```
    60/6000 seconds complete
    1.0 percent complete
    ```

---
+++ countdown_clock.rb
@@ -0,0 +1,19 @@
+puts "Countdown Timer"
+puts "How many Hours:"
+hours = gets.to_i
+puts "How many Minutes:"
+mins = gets.to_i
+puts "How many seconds:"
+secs = gets.to_i
+puts "How many seconds between alerts?"
+interval = gets.to_i
+
+total_time = (hours*60*60)+(mins*60)+secs
+time_elapsed = 0
+
+while(time_elapsed < total_time) do
+  sleep(interval)
+  time_elapsed += interval
+  puts "#{time_elapsed}/#{total_time} seconds complete\n"
+  puts "#{((time_elapsed.to_f/total_time.to_f)*100).round(2)} percent complete\n"
+end
